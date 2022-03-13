ea66ddc44b2007c5103072652fcdbe5ee61cf3c7
AdamTio-18094 <85661116+adamtio@users.noreply.github.com>
2022-03-13 18:55:54 +0700

    Add files via upload

---
+++ FUNGSI.php
@@ -0,0 +1,16 @@
+<?php
+// mmbuat fungsi
+function perkenalan(){
+  echo "Assalamulaikmu, ";
+  echo "Perkenalkan, nama saya adam<br/>";
+  echo "Senang berkenalan dengan anda<br/>";
+}
+
+// memanggil fungsi yang sudah dibuat
+perkenalan();
+
+echo "<hr>";
+
+// memanggilnya lagi
+perkenalan();
+?>
